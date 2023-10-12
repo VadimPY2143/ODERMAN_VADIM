46961b8aaf53a389fbe20f2ecf6fc0f885be99c2
Vadim <vadimpapusha2310@gmail.com>
2023-10-12 21:35:47 +0300

    додав меню

--- main.py
+++ main.py
@@ -9,10 +9,26 @@ app = Flask(__name__)
 
 mas = ['Сьогодні вас чекає гарний день', 'Завтра вас чекає поганий день', 'Вам підвищать зарплату', 'Вас чекають маленькі неприємності']
 
+menu_pizza=[
+    {'pizza': 'Карбонара', 'price': 300},
+    {'pizza': 'Пеппероні', 'price': 250},
+    {'pizza': 'Салямі', 'price': 249},
+    {'pizza': 'Гавайська', 'price': 200}
+]
+
 
 @app.route('/')
 def horoskope():
     return render_template('index.html')
+
+@app.route('/menu/')
+def Menu():
+    context = {
+        'pizzas': menu_pizza
+    }
+    return render_template('menu.html', **context)
+
+
 
 
 if __name__ == '__main__':
